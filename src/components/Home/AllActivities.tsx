0e6ab08790768e9863b9754ca2b1144cbac698e4
Bushido <70598205+Bushidonj@users.noreply.github.com>
2024-09-23 11:36:37 -0300

    update

--- src/components/Home/AllActivities.tsx
+++ src/components/Home/AllActivities.tsx
@@ -1,4 +1,4 @@
-import 'assets/css/stylesAllActivities.css';
+import './assets/css/stylesAllActivities.css';
 import 'assets/css/stylesGlobal.css';
 
 import Carousel from 'components/CarouselAllActivities/Carousel';
