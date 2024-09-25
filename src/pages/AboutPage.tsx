23ef18e4fdc60365a849464533ccb58e6dee1225
Bushido <70598205+Bushidonj@users.noreply.github.com>
2024-09-25 09:56:30 -0300

    update

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,12 +1,19 @@
 import { FooterGlobal, Nav } from 'components';
-import { TransformationStories, LivingTheGospel, MissionAndValues, OurLeaders, BannerBePartOfTheCommunity, BannerAboutUs } from 'components/AboutUsPage';
+import {
+  TransformationStories,
+  LivingTheGospel,
+  MissionAndValues,
+  OurLeaders,
+  BannerBePartOfTheCommunity,
+  BannerAboutUs
+} from 'components/AboutUsPage';
 
 export function AboutPage() {
   return (
     <>
       <Nav />
-      <LivingTheGospel />
-      <BannerAboutUs />
+      {/* <LivingTheGospel /> */}
+      {/* <BannerAboutUs /> */}
       <MissionAndValues />
       <OurLeaders />
       <TransformationStories />
