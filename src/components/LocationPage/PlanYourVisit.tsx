48c356fdc5b009ce7ffea1aed2c80145de5c0fb6
Bushido <70598205+Bushidonj@users.noreply.github.com>
2024-09-26 16:12:43 -0300

    update padding

--- src/components/LocationPage/PlanYourVisit.tsx
+++ src/components/LocationPage/PlanYourVisit.tsx
@@ -16,9 +16,11 @@ export function PlanYourVisit() {
                         <h2 className="globalSectionTitleTextDark">
                             Planeje Sua Visita
                         </h2>
-                        <p className="globalSectionTextDark">
-                            Estamos entusiasmados para recebê-lo na <strong>MountChurch</strong>! Queremos que sua experiência conosco seja <strong>acolhedora</strong> e <strong>significativa</strong> desde o momento em que você chegar.
-                        </p>
+                        <div className='divContainerPlanYourVisit'>
+                            <p className="globalSectionTextDark">
+                                Estamos entusiasmados para recebê-lo na <strong>MountChurch</strong>! Queremos que sua experiência conosco seja <strong>acolhedora</strong> e <strong>significativa</strong> desde o momento em que você chegar.
+                            </p>
+                        </div>
                     </div>
                     <div className='columInfoPlanYourVisit'>
                         <div >
